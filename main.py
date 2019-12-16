fc1f2145d0ab303b99438055c1259d9ce404627b
Jacob Stahl <jacob.stahl@sbcglobal.net>
2019-12-16 17:31:41 -0600

    added more layers to model and added untested dream method

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ import pprint as pp
 import numpy as np
 from set import TextureDataset
 
+def out_size(input_size, kernal_size, stride):
+    out = ((input_size - kernal_size) // stride) + 1
+    print(out)
+    return out
+
 def run():
-    def out_size(input_size, kernal_size, stride):
-        return ((input_size - kernal_size) // stride) + 1
+
     if torch.cuda.is_available():
         device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc. 
         print("Running on GPU")
@@ -40,44 +44,77 @@ def run():
     print("image shape:      ", set.image_shape)
     print("image catagories: ", set.num_catagories)
 
-    class Net(nn.Module):
+    class Net1(nn.Module):
 
         def __init__(self):
             super().__init__()
-            size = set.image_shape[0]
-            self.conv1 = torch.nn.Conv2d(3, 128, kernel_size = 11, stride = 5)
-            size = out_size(size, 11, 5)
-            self.conv2 = torch.nn.Conv2d(128, 512, kernel_size = 5, stride = 2)
-            size = out_size(size, 5, 2)
+            self.size = set.image_shape[0]
+            self.conv1 = torch.nn.Conv2d(3, 512, kernel_size = 11, stride = 7)
+            self.size = out_size(self.size, 11, 7)
+            self.conv2 = torch.nn.Conv2d(512, 512, kernel_size = 5, stride = 2)
+            self.size = out_size(self.size, 5, 2)
+            self.conv3 = torch.nn.Conv2d(512, 1024, kernel_size = 3, stride = 1)
+            self.size = out_size(self.size, 3, 1)
             self.pool1 = torch.nn.MaxPool2d(3)
-            size = size // 3
-            self.conv3 = torch.nn.Conv2d(512, 1024, kernel_size = 5, stride = 2)
-            size = out_size(size, 5, 2)
+            self.size = self.size // 3
+            self.conv4 = torch.nn.Conv2d(1024, 2048, kernel_size = 3, stride = 1)
+            self.size = out_size(self.size, 3, 1)
+            self.pool2 = torch.nn.MaxPool2d(2)
+            self.size = self.size // 2
 
-            self.fc1 = nn.Linear(size*size*1024, 2048)
-            self.fc2 = nn.Linear(2048, 1024)
+            self.fc1 = nn.Linear(self.size*self.size*2048, 1024)
+            self.fc2 = nn.Linear(1024, 1024)
             self.fc3 = nn.Linear(1024, set.num_catagories)
 
-            self.bn1 = nn.BatchNorm1d(2048)
+            self.bn1 = nn.BatchNorm1d(1024)
             self.bn2 = nn.BatchNorm1d(1024)
 
         def forward(self, x):
             x = F.relu(self.conv1(x))
             x = F.relu(self.conv2(x))
-            x = F.relu(self.pool1(x))
             x = F.relu(self.conv3(x))
+            x = F.relu(self.pool1(x))
+            x = F.relu(self.conv4(x))
+            x = F.relu(self.pool2(x))
             x = x.view(x.size()[0], -1)
             x = self.bn1(F.relu(self.fc1(x)))
             x = self.bn2(F.relu(self.fc2(x)))
             x = F.log_softmax(self.fc3(x),-1)
             return x
 
+        def dream(self, x):
+            
+            def deconv(x, weights):
+                x_size = x.size() #(s, s, channels)
+                weights_size =  weights.size() # (f, f, in_channels, out_channels)
+                print("x size: ", x_size, "   w size: ",  weights_size)
+                output = torch.zeros(weights_size[2], x_size[1], x_size[0]) # (channels, x, y)
+                f = weights_size[0]
+                pad = f // 2
+                out_channels = weights_size[3]
+
+                for i in range( x_size[1] - f):
+                    for j in range(x_size[0] - f):
+                        for k in range(out_channels):
+                            output[:, i:(i+f), j:(j+f) ] = output[:, i:(i+f), j:(j+f) ] + weights[:,:,:,k] * x[i, j, k]
+
+                output = output / out_channels
+
+                return output
+
+
+            filt1 = conv1.weight
+            x = F.relu(self.conv1(x))
+
+            
+
+
     print("moving model to device...")
-    net = Net().to(device)
+    net = Net1().to(device)
     opt = optim.Adam(net.parameters(), lr = 0.0001)
     print("done")
     print()
-    epochs = 24
+    epochs = 64
     running_loss = 0.0
 
     for epoch in range(epochs):
@@ -106,7 +143,8 @@ def run():
                     if torch.argmax(i) == Y[idx]:
                         correct += 1
                     total += 1
-            print("acc: ", round(correct/total* 100, 3), "%  cost: ", running_loss)
+            print("acc: ", round(correct/total* 100, 3), "%  cost: ", running_loss * (batch_size / len(train_set)))
+            #plot_filters(net.conv1.weight.cpu().numpy()[1])
             running_loss = 0.0
 
 if __name__ == '__main__':
